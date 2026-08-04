1. Reduce clearance for grips 2. Change order of operations in grip 3. Refactor side='side' to just side 4. Add paper shift over for left corner fold 5. fix long magnet rotation offset

origami/magnets.py
@@ -214,7 +214,7 @@ class LBracketMagnet(Magnet):
         The grip point is offset from the anchor in the direction of
         ``-orientation``.
         """
-        direction = np.array([np.cos(self.orientation), np.sin(self.orientation)])
+        direction = np.array([np.cos(self.orientation), -np.sin(self.orientation)]) # 1/sqrt 2, 1/sqrt 2
         return self.get_anchor_xy() - direction * self.handle_offset
 
     @property